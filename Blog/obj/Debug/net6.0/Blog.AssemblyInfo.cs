6a2fb76e112bd14b1a9b8796506c52049eb23e59
Zak Stanyer <zs262342@falmouth.ac.uk>
2024-11-28 13:42:31 +0000

    sd

--- Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d303895f18921df5a4d3fa9133b62ec389e35c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbddc8d858d6b277ce3ddefcbaf25fe032f7b10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
